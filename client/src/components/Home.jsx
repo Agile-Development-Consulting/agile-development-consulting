b0b422fd6af8367854cf453305111bca6fc41252
jwats287 <jwats287@users.noreply.github.com>
2017-08-23 16:44:22 -0400

    Home component created and running

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                This is my homepage
+            </div>
+        );
+    }
+}
+
+export default Home;
