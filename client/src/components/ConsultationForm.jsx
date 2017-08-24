7a6166a8fcd35a99103fd26e50f9ee70d600ed33
jwats287 <jwats287@users.noreply.github.com>
2017-08-24 14:08:43 -0400

    Added form and styling

--- client/src/components/ConsultationForm.jsx
+++ client/src/components/ConsultationForm.jsx
@@ -6,8 +6,43 @@ class ConsultationForm extends Component {
             <div>
                 <h1>Let’s Get Started with Your Free Consultation!</h1>
                 <form>
+                    <div>
+                        <div className="label">
+                            <label htmlFor="fullName">Full Name</label>
+                        </div>
+                    
+                    <div>
+                    <input type="text" />
+                    </div>
+                    </div>
+                    <div>
+                    <div className="label">
+                    <label htmlFor="email">Full Name</label>
+                    </div>
+                    <div>
+                    <input type="text" />
+                    </div>
+                    </div>
+                    <div>
+                    <div className="label">
+                    <label htmlFor="phone">Full Name</label>
+                    </div>
+                    <div>
+                    <input type="text" />
+                    </div>
+                    </div>
+                    <div>
+                    <div className="label">
+                    <label htmlFor="businessGoal">One Business Goal You Aren't Hitting</label>
+                    </div>
+                    <div>
+                        <textarea class="input" rows="20" cols="100"></textarea>
+                    </div>
+                    </div>
+                    <div>
                     <button>Start Your Free Consultation</button>
-                    </form> 
+                    </div>
+                </form> 
             </div>
         );
     }
