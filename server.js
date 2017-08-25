1d3073cdf85ac994486239ae2353e5a9b5f58491
jwats287 <jwats287@users.noreply.github.com>
2017-08-25 09:55:47 -0400

    Code is in, not working

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+var nodemailer = require('nodemailer');
 const app = express();
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI); //mongodb://localhost/agile-development-consulting
@@ -21,6 +22,48 @@ app.use(express.static(__dirname + '/client/build/'));
 app.get('/', (req,res) => {
     res.sendFile(__dirname + '/client/build/index.html')
   })
+
+
+
+//**********************************//
+//      SAY HELLO WITH NODEMAILER   //
+//**********************************//
+var router = express.Router();
+app.use('/sayHello', router);
+router.post('/', handleSayHello); // handle the route at yourdomain.com/sayHello
+
+function handleSayHello(req, res) {
+    // Not the movie transporter!
+    var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'process.env.GMAIL_EMAIL', // Your email id
+            pass: 'process.env.GMAIL_PASSWORD' // Your password
+        }
+    });
+}
+var text = `Hello world from \n\n + ${req.body.name}`;
+var mailOptions = {
+  from: process.env.GMAIL_EMAIL, // sender address
+  to: process.env.GMAIL_EMAIL, // list of receivers
+  subject: 'Email Example', // Subject line
+  text: text //, // plaintext body
+  // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
+};
+transporter.sendMail(mailOptions, function(error, info){
+  if(error){
+      console.log(error);
+      res.json({yo: 'error'});
+  }else{
+      console.log('Message sent: ' + info.response);
+      res.json({yo: info.response});
+  };
+});
+
+
+//**********************************//
+//          PORT LISTENER           //
+//**********************************//
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
